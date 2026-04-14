95a2846c20755c5fb9972b35aa7d03f8178b5626
Adrian Elder <AdrianLeeElder@gmail.com>
2026-04-14 08:06:03 -0400

    lint: detekt magic number errors

--- backend/src/main/kotlin/com/moneat/config/DemoReseedDashboards.kt
+++ backend/src/main/kotlin/com/moneat/config/DemoReseedDashboards.kt
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+@file:Suppress("MagicNumber")
+
 package com.moneat.config
 
 import com.moneat.dashboards.models.AggFunction
