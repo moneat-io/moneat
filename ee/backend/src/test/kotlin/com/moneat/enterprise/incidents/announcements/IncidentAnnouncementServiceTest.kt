8e0704d96b5d435e62196ab09ac18a55d4f35bbc
Adrian Elder <AdrianLeeElder@gmail.com>
2026-08-27 10:44:27 -0400

    test(incident): keep announcement coverage clean

--- ee/backend/src/test/kotlin/com/moneat/enterprise/incidents/announcements/IncidentAnnouncementServiceTest.kt
+++ ee/backend/src/test/kotlin/com/moneat/enterprise/incidents/announcements/IncidentAnnouncementServiceTest.kt
@@ -20,8 +20,6 @@ import com.moneat.shared.models.OrganizationIntegrations
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.JsonPrimitive
 import org.jetbrains.exposed.v1.core.eq
-import org.jetbrains.exposed.v1.core.inList
-import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.insertAndGetId
 import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
